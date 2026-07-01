70e8903659bd5b119149b97970688f47a592bfd9
Khanh Do <khanh.do@codedok.de>
2026-07-01 20:37:54 +0200

    fix(caps-word): adjust for without german header file

--- keyboards/codedok/sofleplus2/keymaps/tps43-504/keymap.c
+++ keyboards/codedok/sofleplus2/keymaps/tps43-504/keymap.c
@@ -882,14 +882,36 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 void leader_end_user(void) {
     if (leader_sequence_two_keys(KC_S, KC_T)) {
         SEND_STRING(SS_DOWN(X_LGUI));
-        0
         SEND_STRING(SS_DOWN(X_LSFT));
         SEND_STRING(SS_TAP(X_S));      
         SEND_STRING(SS_UP(X_LGUI));
         SEND_STRING(SS_UP(X_LSFT));                                                                                                                                          
     }
 }
-0
+
+
+/* ----------- Caps Word -----------*/
+-------#jfkld#s       
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+        case KC_SLASH:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
 
 // ==================== Keymaps ====================
 
